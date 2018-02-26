6b0c05a8c91a8ed02335a3a46a3a6167747bb3d9
raagn <raagnair@gmail.com>
2018-02-26 00:37:55 -0500

    Respect port env var in WebServer

--- src/com/raagnair/dreamcatcher/server/WebServer.java
+++ src/com/raagnair/dreamcatcher/server/WebServer.java
@@ -11,30 +11,33 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
 
 public class WebServer {
-    public static void main( String[] args ) throws Exception
-    {
-        Server server = new Server(8080);
+	public static int DEFAULT_PORT = 8080;
+	public static String PORT_ENV_VAR = "PORT";
 
-        ServletHandler handler = new ServletHandler();
-        server.setHandler(handler);
+	public static void main(String[] args) throws Exception {
+		String portStr = System.getenv(PORT_ENV_VAR);
+		int port = DEFAULT_PORT;
+		if (portStr != null)
+			port = Integer.parseInt(portStr);
+		Server server = new Server(port);
 
-        handler.addServletWithMapping(HelloServlet.class, "/*");
+		ServletHandler handler = new ServletHandler();
+		server.setHandler(handler);
 
-        server.start();
-        server.join();
-    }
+		handler.addServletWithMapping(HelloServlet.class, "/*");
 
-    @SuppressWarnings("serial")
-    public static class HelloServlet extends HttpServlet
-    {
-        @Override
-        protected void doGet( HttpServletRequest request,
-                              HttpServletResponse response ) throws ServletException,
-                                                            IOException
-        {
-            response.setContentType("text/html");
-            response.setStatus(HttpServletResponse.SC_OK);
-            response.getWriter().println("<h1>Hello from HelloServlet</h1>");
-        }
-    }
+		server.start();
+		server.join();
+	}
+
+	@SuppressWarnings("serial")
+	public static class HelloServlet extends HttpServlet {
+		@Override
+		protected void doGet(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+			response.setContentType("text/html");
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.getWriter().println("<h1>Hello from HelloServlet</h1>");
+		}
+	}
 }
